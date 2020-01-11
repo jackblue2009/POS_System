7c7ba742dd827e286d2c11ccbd2709bba91f8218
Abdulrahman Bucheeri <jack99blue2008@gmail.com>
2020-01-11 18:50:31 +0300

    database updates

---
+++ POS_System/Database/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[order_transactions]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [invoice_no] INT NULL, 
+    [paid_amount] FLOAT NULL, 
+    [pay_method] NCHAR(10) NULL, 
+    [date] DATETIME NULL
+)
